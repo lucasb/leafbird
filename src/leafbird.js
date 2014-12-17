f37c9e216a91a2012e30e655c778761a288ada95
Lucas Boscaini <lucasboscaini@gmail.com>
2014-12-17 19:31:35 -0200

    New fix to find elements in json to print.

--- src/leafbird.js
+++ src/leafbird.js
@@ -12,92 +12,132 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-
 */
 
 (function(arg) {
 
-    this.json;
+  this.json;
+  var dom_objs;
 
-    this.Leafbird = function(arg) {
-        if(!(this instanceof Leafbird)) {
-            return new Leafbird(arg);
+  this.Leafbird = function(arg) {
+    if(!(this instanceof Leafbird)) {
+      return new Leafbird(arg);
+    }
+
+    this.json = arg;
+  };
+
+  Leafbird.prototype.find = function(property, _value, _json) { //TODO: Add contains to find values
+
+    var arr_found = [];
+    json = (_json == undefined) ? this.json : _json;
+
+    if(json.hasOwnProperty(property) && 
+        (_value == undefined || json[property] == _value)) {
+      arr_found.push(json);
+    }
+
+    for(i in json) {
+      if(json[i] instanceof Array) {
+        for(j in json[i]) {
+          found = this.find(property, _value, json[i][j]);
+          if(found.length > 0)
+            arr_found = arr_found.concat(found);
         }
+      }
+    }
 
-        this.json = arg;
-    };
+    return arr_found;
+  };
 
-    Leafbird.prototype.print = function(element, _attr) {
+  Leafbird.prototype.print = function(element, _attr) {
 
-        if(!(element instanceof HTMLElement)) {
-            throw new SyntaxError("Invalid HTMLElement.", element);
+    if(!(element instanceof HTMLElement)) {
+      throw new SyntaxError("Invalid HTMLElement.", element);
+    }
+
+    var reducedJSON = [];
+
+    if(_attr == undefined) {
+      reducedJSON.push(this.json);
+    }
+    else if(_attr.indexOf("#") == 0) {
+      reducedJSON = this.find("id", _attr.substring(1));
+    }
+    else if(_attr.indexOf(".") == 0) {
+      reducedJSON = this.find("class", _attr.substring(1));
+    }
+    else if(_attr.indexOf("$") == 0) {
+      reducedJSON = this.find("label", _attr.substring(1));
+    }
+    else {
+      throw new SyntaxError("JSONAttribute '" + _attr + "' is not valid.", 
+                                                                   _attr);
+    }
+
+    var html = "";
+    for(i in reducedJSON) {
+      html += buildHTML(reducedJSON[i]);
+    }
+
+    element.innerHTML = html;
+  };
+
+  var buildHTML = function(json) {
+
+    var html = "";
+
+    if(json.hasOwnProperty("type")) {
+      html += buildInput(json);
+    }
+    else {
+      for(i in json) {
+        if(json[i] instanceof Array) {
+          for(j in json[i]) {
+            html += buildHTML(json[i][j]);
+          }
         }
+      }
+    }
 
-        var reducedJSON = this.json;
+    return html;
+  };
 
-        if(_attr == undefined) {
-            // undefined print all JSON
-        }
-        else if(_attr.indexOf("#") == 0) { // find id in JSON
-            console.log("id");
-        }
-        else if(_attr.indexOf(".") == 0) { // find label in JSON
-            console.log("label");
-        }
-        else {
-            throw new SyntaxError("JSONAttribute '" + _attr + "' is not valid.",
-                                                                    _attr);
-        }
+  var buildInputElement = function(json) {
 
-        element.innerHTML = buildHTML(reducedJSON);
+    var label = document.createElement("label");
+    label.setAttribute("for", json.id);
+    label.appendChild(document.createTextNode(json.label));
 
-    };
+    var input = document.createElement("input");
+    input.setAttribute("id", json.id);
+    input.setAttribute("name", json.id);
+    input.setAttribute("type", json.type);
+    input.setAttribute("placeholder", json.label);
+  };
 
-    Leafbird.prototype.find = function(property, _value) {
+  var buildInput = function(json, _placeholder) {
 
-        json = this.json;
-        json;
+    // check placeholder and label to genarate html.
+    var html = '<label for="'+json.id+'">'+json.label+'</label>';
 
-    };
+    switch(json.type) {
 
-    var buildHTML = function(json) {
-        json = json.inputs[0];
+      case "text":
+      case "number":
+        html += '<input id="'+json.id+'" name="'+json.id+'" type="'+json.type+'" />';
+        break;
+      case "boolean":
+        break;
+      case "select": // select-check to checkbox
+        break
+      default:
+        throw new SyntaxError("Invalid InputType.", json.type);
+    }
 
-        var html = "";
+    return html;
+  };
 
-        // check if obj is input or group
-        if(json.hasOwnProperty("type")) {
-            html += buildInput(json);
-        }
-        else {
-            // recursive groups
-        }
-
-        return html;
-    };
-
-    var buildInput = function(json, _placeholder) {
-
-        // validar se tem placeholder para gera label ou nao.
-        var html = '<label>'+json.label+'</label>';
-
-        switch(json.type) {
-
-            case "text":
-            case "number":
-                html += '<input id="'+json.id+'" name="'+json.id+'" type="'+json.type+'" />';
-                break;
-            case "boolean":
-                break;
-            case "select":
-                break
-            default:
-                throw new SyntaxError("Invalid InputType.", json.type);
-        }
-
-        return html;
-    };
-
-    window.Leafbird = Leafbird, window.lb = Leafbird;
+  window.Leafbird = Leafbird, window.lb = Leafbird;
 
 })();
