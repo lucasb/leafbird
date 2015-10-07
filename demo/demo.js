9e051ff4489d632a975e151dbd012c3f1cc7f698
Lucas Boscaini <lucasboscaini@gmail.com>
2015-10-07 17:45:09 -0300

    Split script to demo in a new file js.

---
+++ demo/demo.js
@@ -0,0 +1,39 @@
+/*
+    Copyright 2015 Leafbird
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+// get json object from file via jsonp
+var parseResponse = function(data) {
+
+  // start lib to use
+  lb = Leafbird({json: data});
+
+  // configure general paramters after lib was intanced
+  lb.configure({
+    show_group_label: true,
+    show_placeholder: true,
+    show_input_label: true
+  });
+
+  // get a elements object before to set on DOM, to add/change anything
+  console.log(lb.getElements());
+  console.log(lb.find("id", "enable"));
+
+  // get element form from html and print into it
+  var element = document.getElementById("form");
+  lb.print(element, "*.ff", {show_placeholder: false, multifile_input: true});
+  lb.print(element);
+
+};
